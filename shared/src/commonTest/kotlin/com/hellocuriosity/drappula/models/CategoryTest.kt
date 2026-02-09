d414add594943b9519189b1dc20e78c9709f19fd
Kyle Roe <hopeman15@users.noreply.github.com>
2026-02-09 13:08:05 +0100

    fix: dracula attribution url (#56)

--- shared/src/commonTest/kotlin/com/hellocuriosity/drappula/models/CategoryTest.kt
+++ shared/src/commonTest/kotlin/com/hellocuriosity/drappula/models/CategoryTest.kt
@@ -18,7 +18,7 @@ class CategoryTest {
                 category = Category.DRACULA,
                 title = "Dracula",
                 description = "Audio clips sourced from the 1931 Dracula film, which is in the public domain.",
-                url = "https://archive.org/details/dra-cula.-eng.-mp-4",
+                url = "https://archive.org/details/dracula.-1931",
                 license = "Public Domain Mark 1.0",
             ),
         )
